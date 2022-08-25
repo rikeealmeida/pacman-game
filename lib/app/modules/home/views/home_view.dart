c720dc4a069aac1e1aa80093f44d0520f759371a
rikeealmeida <henrique.almeida.ads@gmail.com>
2022-08-25 07:49:15 -0300

    test

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:bonfire/bonfire.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -8,16 +9,11 @@ class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+    return BonfireWidget(
+      map: WorldMapByTiled("map/pacman_map.json",
+          forceTileSize: Vector2(16, 16)),
+      joystick: Joystick(
+        directional: JoystickDirectional(),
       ),
     );
   }
