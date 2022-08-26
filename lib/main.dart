284c570010644566f6e4c0b4aefba8915e9a546f
rikeealmeida <henrique.almeida.ads@gmail.com>
2022-08-26 13:51:10 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Pac man game',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Game(),
+      home: const Game(),
     );
   }
 }
